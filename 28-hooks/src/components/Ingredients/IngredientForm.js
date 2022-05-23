be1ec4ea4c0206f3f66067bd9673db18d9b0b609
jourmooney <jourmooney@gmail.com>
2022-05-24 00:16:36 +0900

    Change folder

--- 28-hooks/src/components/Ingredients/IngredientForm.js
+++ 28-hooks/src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-  const inputState = useState({ title: "", amount: "" });
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
   const submitHandler = (event) => {
     event.preventDefault();
     // ...
@@ -19,12 +19,12 @@ const IngredientForm = React.memo((props) => {
             <input
               type="text"
               id="title"
-              value={inputState[0].title}
+              value={inputState.title}
               onChange={(event) => {
                 const newTitle = event.target.value;
-                inputState[1]({
+                setInputState({
                   title: newTitle,
-                  amount: inputState[0].amount,
+                  amount: inputState.amount,
                 });
               }}
             />
@@ -34,12 +34,12 @@ const IngredientForm = React.memo((props) => {
             <input
               type="number"
               id="amount"
-              value={inputState[0].amount}
+              value={inputState.amount}
               onChange={(event) => {
                 const newAmount = event.target.value;
-                inputState[1]({
+                setInputState({
                   amount: newAmount,
-                  title: inputState[0].title,
+                  title: inputState.title,
                 });
               }}
             />
